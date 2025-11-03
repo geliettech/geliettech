bf3a4f6165ea306d4f86c93abb5c76b2c4add207
julietoge <julietogechi27@gmail.com>
2025-11-03 15:45:30 +0100

    projectSetup

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Sun, Moon } from "lucide-react"
+
+const ThemeToggle = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false)
+
+    return (
+        <button>
+            {isDarkMode ? <Sun /> : <Moon />}
+        </button>
+    )
+}
+
+export default ThemeToggle
---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ThemeToggle from '../components/ThemeToggle'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+        {/* Theme Toggle */}
+        <ThemeToggle />
+        {/* Background Effects */}
+
+        {/* NavBar */}
+
+        {/* Main Content */}
+
+        {/* Footer */}
+        Home
+    </div>
+  )
+}
+
+export default Home

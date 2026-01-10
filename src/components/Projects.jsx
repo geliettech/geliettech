e43d4668d1321981e2023b3308a785f6a91d7ca3
julietoge <julietogechi27@gmail.com>
2026-01-10 16:48:03 +0100

    update contact

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -91,9 +91,9 @@ export const Projects = () => {
         </p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {projects.map((project, key) => (
+          {projects.map((project, index) => (
             <div
-              key={key}
+              key={index}
               className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
             >
               <div className="h-48 overflow-hidden">
